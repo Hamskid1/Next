bcc11740b99722de759f45540ae77d6e13504267
Hamksid <hameedlateef343@gmail.com>
2024-12-27 02:42:46 +0100

    creatin and adding some app and components

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+      <div>
+        ©2024 Lamamia. All rights reseverd.
+      </div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Footer
