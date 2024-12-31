03627f85f8b6ad8ad67111ff827ebc0c6ddfcb76
Hamksid <hameedlateef343@gmail.com>
2024-12-31 03:55:08 +0100

    creating and adding some new content to the project

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import styles from './page.module.css'
+import Button from '@/components/Button/button'
 
-function Category() {
+const Category = ({ params }) => {
+  console.log(params)
   return (
-    <div>Category</div>
+    <div className={styles.container}>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+      <div className={styles.item}>
+        <div className={styles.contents}>
+          <h1 className={styles.title}>title</h1>
+          <p className={styles.desc}>desc</p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            fill={true}
+            src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
   )
 }
 
